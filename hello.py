07467f21c7a372fb88f60f557cb1d0ec543b1096
michael <micfall.romtin@gmail.com>
2016-05-04 01:25:34 +0900

    example of returning http status code

--- hello.py
+++ hello.py
@@ -8,6 +8,10 @@ def index():
     user_agent = request.headers.get('User-Agent')
     return '<h1>Your browser is %s</h1>' % user_agent
 
+@app.route('/bad_request')
+def bad():
+    return '<h1>Bad Request</h1>', 400
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name

10daa8100fb353f39b9164a9cfe92c362c30052e
michael <micfall.romtin@gmail.com>
2016-05-04 03:31:02 +0900

    modify to use flask-bootstrap

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ using virtual environment is recommended.
 $ sudo apt-get install python-virtualenv
 $ virtualenv venv
 $ source ./venv/bin/activate
-(venv)$ pip install flask flask-script
+(venv)$ pip install flask flask-script flask-bootstrap
 ```
--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
-from flask import request
-from flask import make_response
-from flask import redirect
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
